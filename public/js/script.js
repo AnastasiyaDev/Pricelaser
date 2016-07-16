a039d768c233daaf392317f67aec2684b72fb4b1
Anastasiya Bakhnar <katana8832@yandex.ru>
2016-07-16 08:43:51 +0300

    add handler on mobile-menu

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,23 @@
 "use strict";
 
+var foo = {
+    init: function () {
+        $('#moble-menu')
+            .on('click', '.js-open-log-menu', this.openLogMenu.bind(this))
+            .on('click', '.js-open-nav-menu', this.openNavMenu.bind(this));
+	},
 
-$( document ).on( "mousemove", function( event ) {
-  $( "#log" ).text( "pageX: " + event.pageX + ", pageY: " + event.pageY );
-  var nowX = event.pageX;
-  	  nowY = event.pageY;
+	openLogMenu: function (event) {
+		event.preventDefault();
+		$('#log-list').slideToggle('middle');
+		$('#nav-list').slideUp('middle');
+	},
 
+	openNavMenu: function (event) {
+		event.preventDefault();
+		$('#nav-list').slideToggle('middle');
+		$('#log-list').slideUp('middle');
+	},
+};
 
-});
+foo.init();
